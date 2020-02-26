634e87b259eb3061eb930b2bd75798a30aa1ea6c
iblancett <isabel.blancett@students.olin.edu>
2020-02-26 13:36:09 -0500

    Updated fetch_hr.py

--- hr_query/fetch_hr.py
+++ hr_query/fetch_hr.py
@@ -1,24 +1,29 @@
 import dweepy
-import matplotlib.pyplot as plt
-import pandas as pd 
+#import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
 
 time_init = 0
 hr_vals = []
-times = []
+abs_times = []
+rel_times = []
+today = str(datetime.datetime.now().strftime("%Y%m%d")) 
+
 for dweet in dweepy.listen_for_dweets_from('valvefitbit'):
     print(dweet)
-    dweet_dict = dict(dweet)
+    hr_dict = dict(dweet)
     if time_init == 0:
-        time_init = dweet_dict['content']['time']
-    times.append((dweet_dict['content']['time'] - time_init)/1000)
-    hr_vals.append(dweet_dict['content']['hr'])
-    if ((times[-1] - time_init)/1000 >= 780):
+            time_init = hr_dict['content']['time']
+    abs_times.append(hr_dict['content']['time'])
+    rel_times.append((hr_dict['content']['time'] - time_init)/1000)
+    hr_vals.append(hr_dict['content']['hr']) 
+    if (rel_times[-1] >= 30):
         break
 
-heartdf = pd.DataFrame({'Heart Rate':hr_vals,'Time':times})
+heartdf = pd.DataFrame({'Heart Rate':hr_vals,'Abs Time':abs_times, 'Rel Time':rel_times})
 heartdf.to_csv('HR'+ \
                today +'.csv', \
-               columns=['Time','Heart Rate'], header=True, \
+               columns=['Abs Time', 'Rel Time', 'Heart Rate'], header=True, \
                index = False)
 	
     #plt.xlim(0, times[-1])
